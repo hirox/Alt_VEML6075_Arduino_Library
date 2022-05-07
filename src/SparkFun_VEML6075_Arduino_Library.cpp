a8331fc62d27777714e448dc175f985b7c6e6685
Hiromichi Goto <hirox777@gmail.com>
2022-05-07 15:48:29 +0900

    Fix COEF based on the 2018 application note

--- src/SparkFun_VEML6075_Arduino_Library.cpp
+++ src/SparkFun_VEML6075_Arduino_Library.cpp
@@ -72,10 +72,10 @@ const float UV_BETA = 1.0;
 const float UV_GAMMA = 1.0;
 const float UV_DELTA = 1.0;
 
-const float UVA_A_COEF = 2.22;
-const float UVA_B_COEF = 1.33;
-const float UVA_C_COEF = 2.95;
-const float UVA_D_COEF = 1.75;
+const float UVA_A_COEF = 2.22F;
+const float UVA_B_COEF = 1.33F;
+const float UVA_C_COEF = 2.95F;
+const float UVA_D_COEF = 1.74F;
 
 const float UVA_RESPONSIVITY_100MS_UNCOVERED = 0.001111;
 const float UVB_RESPONSIVITY_100MS_UNCOVERED = 0.00125;
